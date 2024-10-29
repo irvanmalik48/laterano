3a4dabd241d5e422ad851e8f0b4cdcbc9dd02605
Irvan Malik <irvanma@gnuweeb.org>
2024-10-29 22:01:54 +0700

    feat(index): initialize paths
    Signed-off-by: Irvan Malik <irvanma@gnuweeb.org>

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import {
+  getAllCommentsByDomainHandler,
+  getAllCommentsByPathHandler,
+  getComment,
+} from "./handlers/getComments.js";
+import {
+  postCommentHandler,
+  deleteCommentHandler,
+  updateCommentHandler,
+} from "./handlers/postComment.js";
 
 const app = new Hono();
 
 app.get("/", (c) => {
-  return c.text("Hello Hono!");
+  return c.text("Server is running.");
 });
+
+app.get("/comments", getAllCommentsByDomainHandler);
+app.get("/comments/:path", getAllCommentsByPathHandler);
+app.get("/comment/:path/:id", getComment);
+app.post("/comment", postCommentHandler);
+app.put("/comment/:path/:id", updateCommentHandler);
+app.delete("/comment/:path/:id", deleteCommentHandler);
 
 const port = 3000;
 console.log(`Server is running on port ${port}`);
